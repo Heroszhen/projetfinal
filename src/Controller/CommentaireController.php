6d950316964b3ab15e2119f60251d8e05b5d4112
Oumaima Fariad <oumaima.fariad@gmail.com>
2019-04-12 16:28:29 +0200

    commentaire en cours 2

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -18,12 +18,27 @@ class CommentaireController extends AbstractController
         // eq de findall() mais avec un tri sur le nom
         $commentaires = $repository->findBy(['article' => $article], ['publicationDate' => 'DESC']);
 
-        return $this->render('admin/comment/index.html.twig',
+        return $this->render('admin/comment/inscription.html.twig',
             [
                 'commentaires' => $commentaires,
                 'article' => $article
             ]);
 
         return $this->render('commentaire/index.html.twig');
+    }
+
+    public function supprimer(Commentaire $commentaire)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $id = $commentaire->getArticle()->getId();
+        $em->remove($commentaire);
+        $em->flush();
+
+        $this->addFlash('success', 'le commentaire est supprimée',
+            [
+                'id' => $id
+            ]);
+        
     }
 }
